bf7dcc89057870dbdce8a8da03afacd245accc87
Massimiliano Amici <m.amici@key2.it>
2024-08-28 19:12:15 +0200

    fix module polybar script

--- polybar/.config/polybar/github_dotfiles.sh
+++ polybar/.config/polybar/github_dotfiles.sh
@@ -4,5 +4,16 @@ cd ~/dotfiles
 gitstatus=$(git status --porcelain=v1 2> /dev/null | wc -l)
 gitcommit=$(git rev-list --right-only --count origin/master..master 2> /dev/null)
 repository="dotfiles:"
-echo "${gitstatus} ${gitcommit}"
+
+if [ "$gitstatus" != 0 ] || [ "$gitcommit" != 0 ]; then
+	if [ "$gitstatus" != 0 ]; then
+		repository="${repository} Modifiche: ${gitstatus}"
+	fi
+	if [ "$gitcommit" != 0 ];then
+		repository="${repository} Commit: ${gitcommit}"
+	fi
+	echo "${repository}"
+else
+	echo ""
+fi
 
